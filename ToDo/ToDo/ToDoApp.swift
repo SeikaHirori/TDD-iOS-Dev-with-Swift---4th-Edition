cff53446263a047ee909daaea5f54a6d7dabd8c2
SeikaHirori <SeikaHirori@outlook.com>
2023-02-07 13:05:26 -0800

    Updated progressoin_textbook.md - Finished Section 1, and starting section 2

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Seika Hirori on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
